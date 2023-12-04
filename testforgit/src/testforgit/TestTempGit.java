37451df27dfbf13b299045557a5d429bc0a5e905
xPY <xPY@DESKTOP-Q8AK6H8.mshome.net>
2023-12-04 21:16:04 +0100

    test with sts

---
+++ testforgit/src/testforgit/TestTempGit.java
@@ -0,0 +1,11 @@
+package testforgit;
+
+public class TestTempGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Salam Ghazza l3izza");
+
+	}
+
+}
